cf13a70e4bc64e5deb94f6b756036c7918e384dd
NattapolDedruktip <dedruktip.n@gmail.com>
2024-12-05 02:39:41 +0700

    list product in shop page

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const { create, list, remove } = require("../controllers/category");
 const { authCheck, adminCheck } = require("../middleware/authCheck");
 const router = express.Router();
 
-router.get("/category", authCheck, adminCheck, list);
+router.get("/category", list);
 router.post("/category", authCheck, adminCheck, create);
 router.delete("/category/:id", authCheck, adminCheck, remove);
 
